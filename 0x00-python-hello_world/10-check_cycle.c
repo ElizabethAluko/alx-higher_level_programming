474f7bb961f754d94b63dce47d8038055221023a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-02 17:13:37 -0800

    Add task10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,8 +3,23 @@
 /**
  * check_cycle - check for a cycle in a linkedlist.
  * @list: linked list.
- * Return: 0 if successful
+ * Return: 1 if it has a cycle, and 0 if not.
  */
 int check_cycle(listint_t *list)
 {
-	
+	listint_t *i = list, *n = list;
+
+	if (!list)
+		return (0);
+	while (i && n && n>next)
+	{
+		i = i->next;
+		n = n->next->next;
+
+		if (i == n)
+			return (1);
+	}
+	return (0);
+}
+
+
