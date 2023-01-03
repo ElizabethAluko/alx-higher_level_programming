6fe7202eb84f8c4e793ac36fc7b15dbc702aee51
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-03 13:59:49 -0800

    Add task8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ((i >= a) and (i <= z)):
+        if ((i >= 'a') and (i <= 'z')):
             print("{}".format(ord(i) -32), end="")
     print("")
