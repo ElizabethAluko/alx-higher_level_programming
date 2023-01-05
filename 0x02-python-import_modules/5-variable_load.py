7f3ed67e965ee4db27f889846d31c138d2afb57f
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-05 15:41:13 -0800

    Add task5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# 5-variable_load.py
 
 if __name__ == "__main__":
-    """import variable and print the value"""
+    """Print the value of variable and a from variable_load_5."""
     from variable_load_5 import a
 
     print(a)
