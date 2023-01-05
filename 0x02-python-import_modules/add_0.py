9ad61965723b4d6ca70e59a324a16ccf23d9b955
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-05 09:43:37 -0800

    Add task0

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-if (__name__ == '__main__'):
-    def add(a, b):
-        """My addition function
-        Args:
-            a: first integer
-            b: second integer
+def add(a, b):
+    """My addition function
 
-        Returns:
-            The return value. a + b
-        """
-        return (a + b)
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
