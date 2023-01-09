31ddaa97500162dbd857401f61d3662713e51f8d
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-09 07:30:39 -0800

    Add task10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2"""
+    for i in my_list:
+        if (i % 2 == 0):
+            my_list[i] = True
+        else:
+            my_list[i] = False
+    return (my_list)
