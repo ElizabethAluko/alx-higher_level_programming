d82c9bd386c02d11da45fa61ecc974c094d7dae8
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-09 10:23:56 -0800

    Add task8

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     """Remove c and C from a string"""
     str = ''
     for i in my_string:
-        if ((i != 'c') and (i != 'C')):
+        if ((i != 'c') or (i != 'C')):
             str += i
     return (str)
