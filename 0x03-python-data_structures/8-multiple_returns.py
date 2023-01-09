1df2acb2d69b2ed6fa0d69ab82703bb7ec432d49
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-09 10:34:16 -0800

    Add task8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence == 0:
+        first = None
+        lenght = 0
+    else:
+        first = sentence[0]
+        lenght = len(sentence)
+    return (firs, lenght)
