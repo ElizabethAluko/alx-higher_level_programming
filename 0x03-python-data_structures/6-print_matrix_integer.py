f3f1bcf3fc27ca85f6380b24b63a5e1ff9660da7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-10 03:57:32 -0800

    Correct task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     """A matrix of integers"""
-    for items in matrix for j in items:
-        if items[-1] == j:
-            print("{:d}".format(j))
-        else:
-            print("{:d}".format(j), end=" "))
+    for items in matrix:
+        for j in items:
+            if items[-1] == j:
+                print("{:d}".format(j))
+            else:
+                print("{:d}".format(j), end=" ")
