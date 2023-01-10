9111286cfa241c1636d0c33b759762e6a6d5a6b8
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-10 09:43:56 -0800

    Add task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,26 +2,26 @@
 
 if __name__ == "__main__":
     """performs some basic calculation"""
-from calculator_1 import add, sub, mul, div
-import sys
-if (len(sys.argv) < 3):
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-if (sys.argv[2] != '+'):
-    if (sys.argv[2] != '-'):
-        if (sys.argv[2] != '*'):
-            if (sys.argv[2] != '/'):
-                print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit(1)
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-operator = sys.argv[2]
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-print("{} {} {} = {}".format(a, operator, b, result))
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if (len(sys.argv) < 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if (sys.argv[2] != '+'):
+        if (sys.argv[2] != '-'):
+            if (sys.argv[2] != '*'):
+                if (sys.argv[2] != '/'):
+                    print("Unknown operator. Available operators: +, -, * and /")
+                    sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
