b2060fdf0e03ff592a738f22a18d1e6dbcf1706e
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-10 13:36:39 -0800

    Fix task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,15 +4,15 @@ if __name__ == "__main__":
     """performs some basic calculation"""
     from calculator_1 import add, sub, mul, div
     import sys
-    if (len(sys.argv) < 3):
+    if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if (sys.argv[2] != '+'):
-        if (sys.argv[2] != '-'):
-            if (sys.argv[2] != '*'):
-                if (sys.argv[2] != '/'):
-                    print("Unknown operator. Available operators: +, -, * and /")
-                    sys.exit(1)
+
+    if (sys.argv[2] != '+' and sys.argv[2] != '-'):
+        if (sys.argv[2] != '*' and sys.argv[2] != '/'):
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = sys.argv[2]
