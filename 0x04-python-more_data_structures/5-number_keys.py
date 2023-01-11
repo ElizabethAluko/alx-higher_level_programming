349c48a2e3c4285a7b95ee6efa72dae31f784301
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-11 07:35:19 -0800

    Add task6

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
+    """ function that returns the number of keys in a dictionary."""
     k = 0
     for i, j in a_dictionary.items():
         k += 1
@@ -11,3 +12,7 @@ def number_keys(a_dictionary):
 #     for i, j in enumerate(a_dictionary):
 #         continue
 #     return (i + 1)
+
+# step3
+# def number_keys(a_dictionary):
+#     return len(a_dictionary.keys())
