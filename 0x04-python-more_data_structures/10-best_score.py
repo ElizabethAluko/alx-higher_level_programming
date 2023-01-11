2b3b220f9e44c663e0dce796a81c8463959de2e9
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-11 12:02:41 -0800

    Add task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    max = 0
+    for k, v in a_dictionary.items():
+        if v > max:
+            v = max
+        else:
+            continue
+    return (sum)
