1da01ea4ae47565b09458a617f8f1a17c1e998ec
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-11 07:20:20 -0800

    Add task5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,6 +2,12 @@
 
 def number_keys(a_dictionary):
     k = 0
-    for i, j in a_dictionary.items:
-        k++
+    for i, j in a_dictionary.items():
+        k += 1
     return (k)
+
+#step2
+#def number_keys(a_dictionary):
+#    for i, j in enumerate(a_dictionary):
+#        continue
+#    return (i + 1)
