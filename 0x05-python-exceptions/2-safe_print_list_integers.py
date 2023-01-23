4fd7cd2d7b5815813852d1e5e3a3b4ababa54cbe
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-23 07:41:01 -0800

    Add task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,11 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
         for i in my_list:
-            if i.isdigit() and count < x:
+            if (i.isdigit()) and (count < x):
                 print("{:d}".fomart(i), end="")
                 count += 1
             else:
                 continue
         print ()
         return (count)
-    except Indexerror:
+    except (ValueError, TypeError):
