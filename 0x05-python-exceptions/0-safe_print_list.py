00dd8cf4b1ee82b0df932c9e03daefa3b4008a48
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-23 07:00:16 -0800

    Add task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         num = 0
 
         for i in range(x):
-        print("{}".format(my_list[i], end=""))
-        num += 1
+            print("{}".format(my_list[i], end=""))
+            num += 1
     except IndexError:
         raise IndexError
     finally:
