14dc2c12f752a281fabf5da9e1d293288a51325a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-23 09:57:07 -0800

    Add task0

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        fun = fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return (None)
+    else:
+        return (fun)
