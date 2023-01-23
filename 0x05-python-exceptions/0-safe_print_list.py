ebead2d95b5181dda03558410dfcd76f3b8b1f76
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-23 06:47:10 -0800

    Add task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i], end=""))
             num += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
     print("")
     return (num)
