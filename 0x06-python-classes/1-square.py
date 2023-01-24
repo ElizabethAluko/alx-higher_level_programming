e32690661daf573fd6176f953aefa24e4a21ed2b
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-24 10:27:17 -0800

    Add task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""Define a Square class"""
+
+
+class Square:
+    """A square has four equal sides"""
+
+    def __init__(self, size):
+       """Create a new Square object.
+
+
+       Args:
+           size (int): The size of the new square object
+     """
+        self.__size = size
