bd59470be1fd8cc555677cde68cdd3f8936a1792
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-24 11:28:15 -0800

    Add task4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+"""Define a Square class"""
+
+
+class Square:
+    """A square has four equal sides"""
+
+    def __init__(self, size=0):
+        """Create a new Square object.
+
+
+        Args:
+            size (int): The size of the new square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Define the area of the square"""
+
+        return (self.__size * self.__size)
+
+    def get__size(self):
+        """It is use to get the __size"""
+        return self.__size
+
+    def set__size(self, size):
+        """It is used to set the value of __size"""
+        self.__size = size
