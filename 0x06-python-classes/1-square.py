ae82f99c2979416205f11c6feee8dd8d7ffcd1b2
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-24 10:38:31 -0800

    Add task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,10 +8,10 @@ class Square:
     """A square has four equal sides"""
 
     def __init__(self, size):
-       """Create a new Square object.
+        """Create a new Square object.
 
 
-       Args:
-           size (int): The size of the new square object
-       """
+        Args:
+            size (int): The size of the new square object
+        """
         self.__size = size
