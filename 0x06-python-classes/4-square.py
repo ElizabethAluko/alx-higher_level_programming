be43aca1459b6d8dfd5bbcf9fb469348c66d6e84
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-24 20:34:25 -0800

    Add task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -28,6 +28,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
+    def area(self):
         """Define the area of the square"""
         return (self.__size * self.__size)
