07bd2341494a78529c9e8e81835d18f6cf1f237d
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-24 20:30:39 -0800

    Add task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,16 +14,7 @@ class Square:
         Args:
             size (int): The size of the new square object
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """Define the area of the square"""
-
-        return (self.__size * self.__size)
+        self.set__size(size)
 
     def get__size(self):
         """It is use to get the __size"""
@@ -36,3 +27,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+        def area(self):
+        """Define the area of the square"""
+        return (self.__size * self.__size)
