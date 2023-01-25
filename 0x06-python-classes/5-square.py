d41eb6a48fba2730593ef56401c0a529ceb3a2ad
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-24 20:52:25 -0800

    Add task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+"""Define a Square class"""
+
+
+class Square:
+    """A square has four equal sides"""
+
+    def __init__(self, size=0):
+        """Create a new Square object.
+
+        Args:
+            size (int): The size of the new square object
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Define the area of the square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with # or dash"""
+        for i in range(self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
