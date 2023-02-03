01ad5f0ef9cd27763dd6a4204db42ca0c82ea9e7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-03 08:50:41 -0800

    Add task

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Contains a function that divides all elements of a matrix."""
+
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix"""
+
+    for i in 
