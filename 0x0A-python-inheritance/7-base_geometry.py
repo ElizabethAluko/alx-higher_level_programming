5659186d4be9a524c2cbed6ad6966c4b7ad08f94
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-06 12:43:23 -0800

    Add task6

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a geometry base class"""
+
+
+class BaseGeometry:
+    """contains geomtry public methods"""
+
+    def area(self):
+        """Not implemented yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the method parameters"""
+        if not isinstance(value,int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
