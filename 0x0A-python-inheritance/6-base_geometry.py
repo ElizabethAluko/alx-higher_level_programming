18a749d4788e98252cfe1e25e3062593437c4d1c
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-06 12:26:01 -0800

    Add task5

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class with area method"""
+
+
+class BaseGeometry:
+    """contains an area class"""
+    def area(self):
+        raise("area() is not implemented")
