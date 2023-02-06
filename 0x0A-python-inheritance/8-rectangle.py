e9819182cc461112dfc3a96a146e7d10f41b5423
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-06 12:53:54 -0800

    Add task8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """Defines a geometry base class"""
-
-
-class BaseGeometry:
-    """contains geomtry public methods"""
-
-    def area(self):
-        """Not implemented yet"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates the method parameters"""
-        if not isinstance(value,int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
