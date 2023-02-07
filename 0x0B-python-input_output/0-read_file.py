7954bd546db3567451025694255df115c28a2e68
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-07 07:45:24 -0800

    Add task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text and print it out"""
+
+
+def read_file(filename=""):
+    """Read a file and print it to stdout"""
+    with open('filename', encoding = "utf-8") as f:
+        read = f.read()
+        print(read)
