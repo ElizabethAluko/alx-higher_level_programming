0333fcb9189a122a76c84cb81b7e482ccd15a78d
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-13 13:10:27 -0800

    Add tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Define a Rectangle class"""
+
 from models.base import Base
 
 
 class Rectangle(Base):
     """Rectangle class"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, 
+            x=0, y=0, id=None):
         """Initiate the rectangle class"""
 
         super().__init__(id)
