90f7d6e0c256d11263eb38a01b751cf867c2e837
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-13 12:50:57 -0800

    Add tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define the base class"""
+import json
 
 
 class Base:
@@ -8,8 +9,16 @@ class Base:
 
     def __init__(self, id=None):
         """Instantiate a new Base"""
+
         if id != None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns json representation of the dictiinary"""
+        if (list_dictionaries is None or list_dictionaries == []):
+            return ("[]")
+        return (json.dumps(list_dictionaries))
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -136,3 +136,13 @@ self.x, self.y)
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Returns dictionary representatio"""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
