a0c613907a9e30c674531fc0b2947a2968b3cf91
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-13 14:13:43 -0800

    Add tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,7 +36,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set the new value of height"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -50,7 +50,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """set the new value of x"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
