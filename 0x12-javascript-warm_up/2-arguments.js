f621b166a37b7ed7a29c911db166b19e67ff0b38
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-04-11 12:50:29 -0700

    Add task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-import { argv } from 'node:process';
+const myVar = process.argv.length;
 
-if (!argv[2]) {
-	console.log('No argument');
-}; else {
-	if (argv[3]) {
-		console.log('Arguments found');
-	}; else {
-		console.log('Argument found');
-	};
-};
+if (myVar < 3) {
+  console.log('No argument');
+} else if (myVar === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
