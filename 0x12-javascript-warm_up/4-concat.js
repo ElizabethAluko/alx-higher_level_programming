96f44af21cb3511dd815b56740b7491ce47b08fd
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-04-11 13:15:54 -0700

    Add task

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,8 @@
 const myVar1 = process.argv[2];
 const myVar2 process.argv[3];
 
-console.log(myVar1 'is' myVar2);
+if (myVar1 >= 3) {
+  console.log(myVar1 'is' myVar2);
+} else {
+  console.log('undefined is' myVar2);
+}
