0b37c6e67a80702e269035b206c999d337fc0446
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-04-15 12:54:26 -0700

    Add tasks

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,10 @@ def main():
             )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id,\
+            cities.name, states.name FROM cities\
+            INNER JOIN states ON\
+            cities.state_id=states.id ORDER BY cities.id")
     rows = cur.fetchall()
 
     for row in rows:
