36951153eef2b7ab86db3fe6b5a7d86cc467f56c
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-04-15 11:07:09 -0700

    Add tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(host='localhost', port=3306, user=mysql_user,
-                         passwd=mysql_password, db=db_name)
+            passwd=mysql_password, db=db_name)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
