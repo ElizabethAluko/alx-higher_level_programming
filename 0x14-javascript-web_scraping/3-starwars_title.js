f97d0b5f4fd86037c127767929ecd2f9e5b3657d
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-05-23 10:30:35 -0700

    Add task

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Check if a movie ID is provided as an argument
+if (process.argv.length < 3) {
+  console.log("Please provide a movie ID as an argument.");
+  process.exit(1);
+}
+
+// Get the movie ID from the command line argument
+const movieId = process.argv[2];
+
+// Construct the API endpoint URL
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make the API request
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(`An error occurred while making the API request: ${error.message}`);
+  } else {
+    if (response.statusCode === 200) {
+      const movie = JSON.parse(body);
+      console.log(`Title: ${movie.title}`);
+    } else {
+      console.error(`Failed to fetch movie details. Status code: ${response.statusCode}`);
+    }
+  }
+});
