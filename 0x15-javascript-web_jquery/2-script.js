1fc251e1548d3b0f178428a7404930f10061a2af
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-05-31 23:09:09 +0100

    Add task

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Attach a click event handler to the <div> with ID "red_header"
+$('#red_header').click(function() {
+  // Update the text color of the <header> element to red (#FF0000)
+  $('header').css('color', '#FF0000');
+});
